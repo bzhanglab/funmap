145acaf710438022cf180a5065bb5cf201532d84
Zhiao Shi <zhiao.shi@gmail.com>
2023-04-28 12:43:53 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
@@ -43,7 +40,7 @@ setup(
     },
     install_requires=requirements,
     license='MIT license',
-    long_description=readme + '\n\n' + history,
+    long_description='\n\n' + history,
     include_package_data=True,
     keywords=['funmap', 'bioinformatics', 'biological-network'],
     name='funmap',
