9e534788ccd3e3ae6fac594d1b744138ba4e1987
John Elizarraras <elizarrj27@gmail.com>
2024-09-27 16:54:53 -0500

    formatting changes

--- python/funmap/data_urls.py
+++ python/funmap/data_urls.py
@@ -1,17 +1,18 @@
 misc_urls = {
-    'reactome_gold_standard': 'https://figshare.com/ndownloader/files/38647601',
-    'reactome_gold_standard_md5': '671942763b6a7c32506cba1ed9900fe6',
-    'funmap_blacklist': 'https://figshare.com/ndownloader/files/39033977',
-    'mapping_file': 'https://figshare.com/ndownloader/files/39033971'
+    "reactome_gold_standard": "https://figshare.com/ndownloader/files/38647601",
+    "reactome_gold_standard_md5": "671942763b6a7c32506cba1ed9900fe6",
+    "funmap_blacklist": "https://figshare.com/ndownloader/files/39033977",
+    "mapping_file": "https://figshare.com/ndownloader/files/39033971",
 }
 
 # the information about other networks is fixed for now
 network_info = {
-    'name': ['BioGRID', 'BioPlex', 'HI-union', 'STRING'],
-    'type': ['BioGRID', 'BioPlex', 'HI', 'STRING'],
-    'url': ['https://figshare.com/ndownloader/files/39125054',
-            'https://figshare.com/ndownloader/files/39125051',
-            'https://figshare.com/ndownloader/files/39125093',
-            'https://figshare.com/ndownloader/files/39125090'
-        ]
+    "name": ["BioGRID", "BioPlex", "HI-union", "STRING"],
+    "type": ["BioGRID", "BioPlex", "HI", "STRING"],
+    "url": [
+        "https://figshare.com/ndownloader/files/39125054",
+        "https://figshare.com/ndownloader/files/39125051",
+        "https://figshare.com/ndownloader/files/39125093",
+        "https://figshare.com/ndownloader/files/39125090",
+    ],
 }
