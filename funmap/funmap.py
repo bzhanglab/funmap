01e89387723ce676a26c8bc2b90e5f7d75868264
John Elizarraras <elizarrj27@gmail.com>
2024-12-04 17:03:14 -0600

    save all_ids

--- funmap/funmap.py
+++ funmap/funmap.py
@@ -755,6 +755,9 @@ def predict_all_pairs(
     chunk_size = 1000000
     log.info("Genearating all pairs ...")
     all_ids = sorted(all_ids)
+    all_id_file = prediction_dir / "all_ids.pkl"
+    with gzip.open(all_id_file, "wb") as fh:
+        pickle.dump(all_ids, fh)
     all_pairs = list(itertools.combinations(all_ids, 2))
     log.info("Genearating all pairs ... done")
     log.info(f'Number of valid ids {format(len(all_ids), ",")}')
