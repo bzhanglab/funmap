2e6c5ad9ff8153d8c69bccb586220fc178fa16cf
John Elizarraras <elizarrj27@gmail.com>
2026-01-15 10:27:18 -0600

    Fixes issue caused by wrong figshare url

--- funmap/data_urls.py
+++ funmap/data_urls.py
@@ -1,8 +1,8 @@
 misc_urls = {
-    "reactome_gold_standard": "https://figshare.com/ndownloader/files/38647601",
+    "reactome_gold_standard": "https://api.figshare.com/v2/file/download/38647601",
     "reactome_gold_standard_md5": "671942763b6a7c32506cba1ed9900fe6",
-    "funmap_blacklist": "https://figshare.com/ndownloader/files/39033977",
-    "mapping_file": "https://figshare.com/ndownloader/files/39033971",
+    "funmap_blacklist": "https://api.figshare.com/v2/file/download/9033977",
+    "mapping_file": "https://api.figshare.com/v2/file/download/39033971",
 }
 
 # the information about other networks is fixed for now
@@ -10,9 +10,9 @@ network_info = {
     "name": ["BioGRID", "BioPlex", "HI-union", "STRING"],
     "type": ["BioGRID", "BioPlex", "HI", "STRING"],
     "url": [
-        "https://figshare.com/ndownloader/files/39125054",
-        "https://figshare.com/ndownloader/files/39125051",
-        "https://figshare.com/ndownloader/files/39125093",
-        "https://figshare.com/ndownloader/files/39125090",
+        "https://api.figshare.com/v2/file/download/39125054",
+        "https://api.figshare.com/v2/file/download/39125051",
+        "https://api.figshare.com/v2/file/download/39125093",
+        "https://api.figshare.com/v2/file/download/39125090",
     ],
 }
